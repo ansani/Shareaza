d27ce83fa86f031aa02c15d6952353ec11d363fb
Restyled.io <commits@restyled.io>
2022-10-25 07:45:36 +0000

    Restyled by shfmt

--- MiniUPnPc/miniupnpc/testminiwget.sh
+++ MiniUPnPc/miniupnpc/testminiwget.sh
@@ -17,11 +17,11 @@
 # The script was tested and works with ksh, bash
 # it should now also run with dash
 
-TMPD=`mktemp -d -t miniwgetXXXXXXXXXX`
-if [ -z "$TESTSERVER" ] ; then
+TMPD=$(mktemp -d -t miniwgetXXXXXXXXXX)
+if [ -z "$TESTSERVER" ]; then
   TESTSERVER=./build/minihttptestserver
 fi
-if [ -z "$TESTMINIWGET" ] ; then
+if [ -z "$TESTMINIWGET" ]; then
   TESTMINIWGET=./build/testminiwget
 fi
 HTTPSERVEROUT="$TMPD/httpserverout"
@@ -31,51 +31,51 @@ PORT=
 RET=0
 IPCONFIG=$(which ifconfig)
 IP=$(which ip)
-if [ "$IP" ] ; then
-	if ! "$IP" addr | grep inet6 ; then
-		HAVE_IPV6=no
-	fi
+if [ "$IP" ]; then
+  if ! "$IP" addr | grep inet6; then
+    HAVE_IPV6=no
+  fi
 else
-	if [ -z "$IPCONFIG" ] ; then
-		IPCONFIG="/sbin/ifconfig"
-	fi
+  if [ -z "$IPCONFIG" ]; then
+    IPCONFIG="/sbin/ifconfig"
+  fi
 
-	if ! "$IPCONFIG" -a | grep inet6 ; then
-		HAVE_IPV6=no
-	fi
+  if ! "$IPCONFIG" -a | grep inet6; then
+    HAVE_IPV6=no
+  fi
 fi
 
 case "$HAVE_IPV6" in
-    n|no|0)
-        ADDR=localhost
-        SERVERARGS=""
-        ;;
-    *)
-        ADDR="[::1]"
-        SERVERARGS="-6"
-        ;;
+  n | no | 0)
+    ADDR=localhost
+    SERVERARGS=""
+    ;;
+  *)
+    ADDR="[::1]"
+    SERVERARGS="-6"
+    ;;
 
 esac
 
-if [ ! -x "$TESTSERVER" ] || [ ! -x "$TESTMINIWGET" ] ; then
-	echo "Please build $TESTSERVER and $TESTMINIWGET"
-	#make minihttptestserver
-	#make testminiwget
-	exit 1
+if [ ! -x "$TESTSERVER" ] || [ ! -x "$TESTMINIWGET" ]; then
+  echo "Please build $TESTSERVER and $TESTMINIWGET"
+  #make minihttptestserver
+  #make testminiwget
+  exit 1
 fi
 
 # launching the test HTTP server
-"$TESTSERVER" "$SERVERARGS" -e "$EXPECTEDFILE" > "$HTTPSERVEROUT" &
+"$TESTSERVER" "$SERVERARGS" -e "$EXPECTEDFILE" >"$HTTPSERVEROUT" &
 SERVERPID=$!
 while [ -z "$PORT" ]; do
-	sleep 1
-	PORT=`cat "$HTTPSERVEROUT" | sed 's/Listening on port \([0-9]*\)/\1/' `
+  sleep 1
+  PORT=$(cat "$HTTPSERVEROUT" | sed 's/Listening on port \([0-9]*\)/\1/')
 done
 if [ "$PORT" = "*** ERROR ***" ]; then
-	echo "HTTP test server error"
-	echo "Network config :"
-	"$IPCONFIG" -a
-	exit 2
+  echo "HTTP test server error"
+  echo "Network config :"
+  "$IPCONFIG" -a
+  exit 2
 fi
 echo "Test HTTP server is listening on $PORT"
 
@@ -86,29 +86,29 @@ URL4="http://$ADDR:$PORT/malformed"
 
 echo "standard test ..."
 "$TESTMINIWGET" "$URL1" "$DOWNLOADEDFILE.1"
-if cmp "$EXPECTEDFILE" "$DOWNLOADEDFILE.1" ; then
-	echo "ok"
+if cmp "$EXPECTEDFILE" "$DOWNLOADEDFILE.1"; then
+  echo "ok"
 else
-	echo "standard test FAILED"
-	RET=1
+  echo "standard test FAILED"
+  RET=1
 fi
 
 echo "chunked transfert encoding test ..."
 "$TESTMINIWGET" "$URL2" "$DOWNLOADEDFILE.2"
-if cmp "$EXPECTEDFILE" "$DOWNLOADEDFILE.2" ; then
-	echo "ok"
+if cmp "$EXPECTEDFILE" "$DOWNLOADEDFILE.2"; then
+  echo "ok"
 else
-	echo "chunked transfert encoding test FAILED"
-	RET=1
+  echo "chunked transfert encoding test FAILED"
+  RET=1
 fi
 
 echo "response too long test ..."
 "$TESTMINIWGET" "$URL3" "$DOWNLOADEDFILE.3"
-if cmp "$EXPECTEDFILE" "$DOWNLOADEDFILE.3" ; then
-	echo "ok"
+if cmp "$EXPECTEDFILE" "$DOWNLOADEDFILE.3"; then
+  echo "ok"
 else
-	echo "response too long test FAILED"
-	RET=1
+  echo "response too long test FAILED"
+  RET=1
 fi
 
 echo "malformed response test ..."
@@ -120,14 +120,13 @@ wait "$SERVERPID"
 
 # remove temporary files (for success cases)
 if [ "$RET" -eq 0 ]; then
-	rm -f "$DOWNLOADEDFILE.1"
-	rm -f "$DOWNLOADEDFILE.2"
-	rm -f "$DOWNLOADEDFILE.3"
-	rm -f "$EXPECTEDFILE" "$HTTPSERVEROUT"
-	rmdir "$TMPD"
+  rm -f "$DOWNLOADEDFILE.1"
+  rm -f "$DOWNLOADEDFILE.2"
+  rm -f "$DOWNLOADEDFILE.3"
+  rm -f "$EXPECTEDFILE" "$HTTPSERVEROUT"
+  rmdir "$TMPD"
 else
-	echo "at least one of the test FAILED"
-	echo "directory $TMPD is left intact"
+  echo "at least one of the test FAILED"
+  echo "directory $TMPD is left intact"
 fi
 exit "$RET"
-
--- MiniUPnPc/miniupnpc/updateminiupnpcstrings.sh
+++ MiniUPnPc/miniupnpc/updateminiupnpcstrings.sh
@@ -6,62 +6,61 @@
 FILE=miniupnpcstrings.h
 TEMPLATE_FILE=$FILE.in
 
-if [ -n "$1" ] ; then
+if [ -n "$1" ]; then
   FILE="$1"
 fi
-if [ -n "$2" ] ; then
+if [ -n "$2" ]; then
   TEMPLATE_FILE="$2"
 fi
-TMPFILE=`mktemp -t miniupnpcstringsXXXXXX`
-if [ ! -f "$TMPFILE" ] ; then
-	echo "mktemp failure"
-	exit 1
+TMPFILE=$(mktemp -t miniupnpcstringsXXXXXX)
+if [ ! -f "$TMPFILE" ]; then
+  echo "mktemp failure"
+  exit 1
 fi
 
 # detecting the OS name and version
-OS_NAME=`uname -s`
-OS_VERSION=`uname -r`
+OS_NAME=$(uname -s)
+OS_VERSION=$(uname -r)
 if [ -f /etc/debian_version ]; then
-	OS_NAME=Debian
-	OS_VERSION=`cat /etc/debian_version`
+  OS_NAME=Debian
+  OS_VERSION=$(cat /etc/debian_version)
 fi
 
 # use lsb_release (Linux Standard Base) when available
-LSB_RELEASE=`which lsb_release`
+LSB_RELEASE=$(which lsb_release)
 if [ 0 -eq $? -a -x "$LSB_RELEASE" ]; then
-	# On NixOS, lsb_release returns strings such as "NixOS" (with quotes),
-	# so we need to stript them with the following xargs trick:
-	OS_NAME=`"$LSB_RELEASE" -i -s | xargs echo`
-	OS_VERSION=`"$LSB_RELEASE" -r -s | xargs echo`
-	case $OS_NAME in
-		Debian)
-			#OS_VERSION=`${LSB_RELEASE} -c -s`
-			;;
-		Ubuntu)
-			#OS_VERSION=`${LSB_RELEASE} -c -s`
-			;;
-	esac
+  # On NixOS, lsb_release returns strings such as "NixOS" (with quotes),
+  # so we need to stript them with the following xargs trick:
+  OS_NAME=$("$LSB_RELEASE" -i -s | xargs echo)
+  OS_VERSION=$("$LSB_RELEASE" -r -s | xargs echo)
+  case $OS_NAME in
+    Debian)
+      #OS_VERSION=`${LSB_RELEASE} -c -s`
+      ;;
+    Ubuntu)
+      #OS_VERSION=`${LSB_RELEASE} -c -s`
+      ;;
+  esac
 fi
 
 # on AmigaOS 3, uname -r returns "unknown", so we use uname -v
 if [ "$OS_NAME" = "AmigaOS" ]; then
-	if [ "$OS_VERSION" = "unknown" ]; then
-		OS_VERSION=`uname -v`
-	fi
+  if [ "$OS_VERSION" = "unknown" ]; then
+    OS_VERSION=$(uname -v)
+  fi
 fi
 
 echo "Detected OS [$OS_NAME] version [$OS_VERSION]"
-MINIUPNPC_VERSION=`cat VERSION`
+MINIUPNPC_VERSION=$(cat VERSION)
 echo "MiniUPnPc version [$MINIUPNPC_VERSION]"
 
 EXPR="s|OS_STRING \".*\"|OS_STRING \"$OS_NAME/$OS_VERSION\"|"
 #echo $EXPR
 test -f "$FILE".in
 echo "setting OS_STRING macro value to $OS_NAME/$OS_VERSION in $FILE."
-sed -e "$EXPR" < "$TEMPLATE_FILE" > "$TMPFILE"
+sed -e "$EXPR" <"$TEMPLATE_FILE" >"$TMPFILE"
 
 EXPR="s|MINIUPNPC_VERSION_STRING \".*\"|MINIUPNPC_VERSION_STRING \"$MINIUPNPC_VERSION\"|"
 echo "setting MINIUPNPC_VERSION_STRING macro value to $MINIUPNPC_VERSION in $FILE."
-sed -e "$EXPR" < "$TMPFILE" > "$FILE"
+sed -e "$EXPR" <"$TMPFILE" >"$FILE"
 rm "$TMPFILE" && echo "$TMPFILE deleted"
-
