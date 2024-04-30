ad4457bc11626293d4dc9c82656cbed46f37a483
Salvatore Ansani <salvatore.ansani@amdigitallife.it>
2024-04-30 11:57:41 +0200

    Upgraded Boost and 7z NuGet packages

--- 7z.cmd
+++ 7z.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set app="..\vc143\packages\7-Zip.StandaloneConsole.x64.22.1.0\tools\7za.exe"
+set app="..\vc143\packages\7-Zip.StandaloneConsole.x64.23.1.0\tools\7za.exe"
 
 %app% a -tgzip -mx=9 -- %2 %1
 if %errorlevel% equ 9009 echo The 7-Zip utility is missing. Please go to http://www.7-zip.org/ and install 7-Zip.
